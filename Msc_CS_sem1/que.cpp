be913034eb2757f36065f970b5abe0f6660630a2
Anurag Kulkarni <anukul984@gmail.com>
2023-09-03 05:07:51 +0000

    edited:circular que

--- Msc_CS_sem1/que.cpp
+++ Msc_CS_sem1/que.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int number[5];
 int rear = -1, front=0,elements=0;
 bool empty(){
-    if((rear+1)==front)
+    if(elements==0)
         return true;
     else
         return false;
 }
 
 bool full(){
-    if(rear==4)
+    if(elements==5)
         return true;
     
     else
@@ -25,7 +25,9 @@ void deque(){
    }
    else{
    int val=number[front];
-   front+=1;
+   //front+=1;
+   front+=1%5;  //circular que
+   elements--;
    cout<<"removed "<<val<<endl;
    }
 }
@@ -34,8 +36,10 @@ void enque(int val)
    if(full()){
       cout<<"que is full"<<endl;
    }else{
-   rear+=1;
+    //rear+=1;
+   rear+=1%5;   //circular que
    number[rear]=val;
+   elements++;
    cout<<"inserted:"<<val<<endl;
    }
 }
@@ -46,4 +50,11 @@ int main(){
  enque(89);
  deque();
  deque();
+ deque();
+ enque(5);
+ enque(6);
+ deque();
+ deque();
+ enque(3);
+
 }
